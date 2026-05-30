ee30160b38bc9d7956821d176bf3a38bfd9455e0
jaraalan85-ops <jaraalan85@gmail.com>
2026-05-30 15:15:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-const enterBtn = document.getElementById("enterBtn");
-const intro = document.getElementById("intro");
-const album = document.getElementById("album");
-
-enterBtn.addEventListener("click", () => {
-
-    intro.style.opacity = "0";
-
-    setTimeout(() => {
-        intro.style.display = "none";
-        album.classList.remove("hidden");
-    }, 1000);
-
-});
+alert("app.js cargó correctamente");
